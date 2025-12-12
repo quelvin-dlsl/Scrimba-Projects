85c09894137e59f3c6fca071eb6c4e1ff65c8608
quelvin-dlsl <qnaqyeon@gmail.com>
2025-12-13 00:24:27 +0400

    Script to add all posts from array
    Used for loop. InnerHTML to append new text and images into the main section

--- instagram_clone/index.js
+++ instagram_clone/index.js
@@ -28,3 +28,72 @@ const posts = [
     }
 ]
 
+const postAvatar = document.getElementById("post-avatar")
+const postUsername = document.getElementById("post-username")
+const postLocation = document.getElementById("post-location")
+
+const postImage = document.getElementById("post-image")
+const numLikes = document.getElementById("num-likes")
+
+const username = document.getElementById("user-id")
+const comment = document.getElementById("comment")
+
+let feed = document.getElementById("feed")
+let firstPost = 0
+
+console.log(feed)
+
+for (let i = 0; i < posts.length; i++){
+    // postAvatar.src += posts[i].avatar
+
+    // postUsername.textContent += posts[i].name
+    // postLocation.textContent += posts[i].location
+
+    let lastIndex = posts.length - 1
+
+    if (firstPost === i) {
+        feed.innerHTML = `
+            <section class="container">
+                <div class="post-header">
+                    <img id="post-avatar" src="${postAvatar.src = posts[i].avatar}">
+                    <div class="block">
+                        <h3 id="post-username">${postUsername.textContent = posts[i].name}</h3>
+                        <p id="post-location">${postLocation.textContent = posts[i].location}</p>
+                    </div>
+                </div>
+                <img id="post-image" src="${postImage.src = posts[i].post}">
+                <div class="post-footer">
+                    <div class="post-icons">
+                        <img id="like-icon" src="images/icon-heart.png">
+                        <img id="comment-icon" src="images/icon-comment.png">
+                        <img id="dm-icon" src="images/icon-dm.png">
+                    </div>
+                    <p id="num-likes">${numLikes.textContent = posts[i].likes} likes</p>
+                    <!-- caption? -->
+                    <p><span id="user-id">${username.textContent = posts[i].username}</span> <span id="comment">${comment.textContent = posts[i].comment}</span></p>
+                </div>
+            </section>`
+    } else if (i <= lastIndex) {
+        feed.innerHTML += `
+            <section class="container">
+                <div class="post-header">
+                    <img id="post-avatar" src="${postAvatar.src = posts[i].avatar}">
+                    <div class="block">
+                        <h3 id="post-username">${postUsername.textContent = posts[i].name}</h3>
+                        <p id="post-location">${postLocation.textContent = posts[i].location}</p>
+                    </div>
+                </div>
+                <img id="post-image" src="${postImage.src = posts[i].post}">
+                <div class="post-footer">
+                    <div class="post-icons">
+                        <img id="like-icon" src="images/icon-heart.png">
+                        <img id="comment-icon" src="images/icon-comment.png">
+                        <img id="dm-icon" src="images/icon-dm.png">
+                    </div>
+                    <p id="num-likes">${numLikes.textContent = posts[i].likes} likes</p>
+                    <!-- caption? -->
+                    <p><span id="user-id">${username.textContent = posts[i].username}</span> <span id="comment">${comment.textContent = posts[i].comment}</span></p>
+                </div>
+            </section>`
+    }
+}
