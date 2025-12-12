d78c96a77bf33682834df6a61435b4fb974bb643
quelvin-dlsl <qnaqyeon@gmail.com>
2025-12-13 01:51:46 +0400

    Change selector to class and added index
    Selector for the image like icon is changed to class.
    
    Used classList.contains method to target intended image and .getAttribute method to target indexed icons within the loop (Comments are added in js file)
    
    This feature makes like icon clickable and image post double clickable and increments number of likes

--- instagram_clone/index.js
+++ instagram_clone/index.js
@@ -33,24 +33,15 @@ const postUsername = document.getElementById("post-username")
 const postLocation = document.getElementById("post-location")
 
 const postImage = document.getElementById("post-image")
-const numLikes = document.getElementById("num-likes")
 
 const username = document.getElementById("user-id")
 const comment = document.getElementById("comment")
 
 let feed = document.getElementById("feed")
 let firstPost = 0
-
-console.log(feed)
+let lastIndex = posts.length - 1
 
 for (let i = 0; i < posts.length; i++){
-    // postAvatar.src += posts[i].avatar
-
-    // postUsername.textContent += posts[i].name
-    // postLocation.textContent += posts[i].location
-
-    let lastIndex = posts.length - 1
-
     if (firstPost === i) {
         feed.innerHTML = `
             <section class="container">
@@ -61,18 +52,20 @@ for (let i = 0; i < posts.length; i++){
                         <p id="post-location">${postLocation.textContent = posts[i].location}</p>
                     </div>
                 </div>
-                <img id="post-image" src="${postImage.src = posts[i].post}">
+                <img class="post-image" data-index="${i}" src="${postImage.src = posts[i].post}">
                 <div class="post-footer">
                     <div class="post-icons">
-                        <img id="like-icon" src="images/icon-heart.png">
+                        <img class="like-icon" data-index="${i}" src="images/icon-heart.png">
                         <img id="comment-icon" src="images/icon-comment.png">
                         <img id="dm-icon" src="images/icon-dm.png">
                     </div>
-                    <p id="num-likes">${numLikes.textContent = posts[i].likes} likes</p>
+                    <p class="num-likes" id="likes-${i}">${posts[i].likes} likes</p>
                     <!-- caption? -->
                     <p><span id="user-id">${username.textContent = posts[i].username}</span> <span id="comment">${comment.textContent = posts[i].comment}</span></p>
                 </div>
             </section>`
+
+
     } else if (i <= lastIndex) {
         feed.innerHTML += `
             <section class="container">
@@ -83,17 +76,39 @@ for (let i = 0; i < posts.length; i++){
                         <p id="post-location">${postLocation.textContent = posts[i].location}</p>
                     </div>
                 </div>
-                <img id="post-image" src="${postImage.src = posts[i].post}">
+                <img class="post-image" data-index="${i}" src="${postImage.src = posts[i].post}">
                 <div class="post-footer">
                     <div class="post-icons">
-                        <img id="like-icon" src="images/icon-heart.png">
+                        <img class="like-icon" data-index="${i}" src="images/icon-heart.png">
                         <img id="comment-icon" src="images/icon-comment.png">
                         <img id="dm-icon" src="images/icon-dm.png">
                     </div>
-                    <p id="num-likes">${numLikes.textContent = posts[i].likes} likes</p>
+                    <p class="num-likes" id="likes-${i}">${posts[i].likes} likes</p>
                     <!-- caption? -->
                     <p><span id="user-id">${username.textContent = posts[i].username}</span> <span id="comment">${comment.textContent = posts[i].comment}</span></p>
                 </div>
             </section>`
     }
 }
+
+// Used classList.contains method to target intended image and .getAttribute method to target unique indexed icons within the loop
+feed.addEventListener("click", function(){
+    if(event.target.classList.contains("like-icon")) {
+        let index = event.target.getAttribute("data-index");
+
+        posts[index].likes++
+
+        document.getElementById(`likes-${index}`).textContent = `${posts[index].likes} likes`
+    }
+})
+
+feed.addEventListener("dblclick", function(){
+    if(event.target.classList.contains("post-image")) {
+        let index = event.target.getAttribute("data-index");
+
+        posts[index].likes++
+
+        document.getElementById(`likes-${index}`).textContent = `${posts[index].likes} likes`
+    }
+})
+
