0fc3237db1d2468ed445d3b6b172b8046cf15af0
quelvin-dlsl <qnaqyeon@gmail.com>
2025-12-15 18:31:34 +0400

    Refactored logic of like feature and double click image to like
    Used forEach method and used uuid to uniquely identify each post. Now needs to run on a live server

---
+++ instagram_clone/data.js
@@ -0,0 +1,35 @@
+export const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21,
+        isLiked: 0,
+        uuid: '196de92b-57c6-4ce4-9341-f091c9dc9f1f'
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4,
+        isLiked: 0,
+        uuid: '022477ef-98df-4d4a-8f6e-4ea4fd92d8a5'
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152,
+        isLiked: 0,
+        uuid: '6159c776-b1d3-4528-924f-5d57f1e8a6a8'
+    }
+]
--- instagram_clone/index.js
+++ instagram_clone/index.js
@@ -1,114 +1,86 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+import { posts } from './data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+        handleLikeClick(e.target.dataset.like)
     }
-]
+})
 
-const postAvatar = document.getElementById("post-avatar")
-const postUsername = document.getElementById("post-username")
-const postLocation = document.getElementById("post-location")
-
-const postImage = document.getElementById("post-image")
-
-const username = document.getElementById("user-id")
-const comment = document.getElementById("comment")
-
-let feed = document.getElementById("feed")
-let firstPost = 0
-let lastIndex = posts.length - 1
-
-for (let i = 0; i < posts.length; i++){
-    if (firstPost === i) {
-        feed.innerHTML = `
-            <section class="container">
-                <div class="post-header">
-                    <img id="post-avatar" src="${postAvatar.src = posts[i].avatar}">
-                    <div class="block">
-                        <h3 id="post-username">${postUsername.textContent = posts[i].name}</h3>
-                        <p id="post-location">${postLocation.textContent = posts[i].location}</p>
-                    </div>
-                </div>
-                <img class="post-image" data-index="${i}" src="${postImage.src = posts[i].post}">
-                <div class="post-footer">
-                    <div class="post-icons">
-                        <img class="like-icon" data-index="${i}" src="images/icon-heart.png">
-                        <img id="comment-icon" src="images/icon-comment.png">
-                        <img id="dm-icon" src="images/icon-dm.png">
-                    </div>
-                    <p class="num-likes" id="likes-${i}">${posts[i].likes} likes</p>
-                    <!-- caption? -->
-                    <p><span id="user-id">${username.textContent = posts[i].username}</span> <span id="comment">${comment.textContent = posts[i].comment}</span></p>
-                </div>
-            </section>`
-
-
-    } else if (i <= lastIndex) {
-        feed.innerHTML += `
-            <section class="container">
-                <div class="post-header">
-                    <img id="post-avatar" src="${postAvatar.src = posts[i].avatar}">
-                    <div class="block">
-                        <h3 id="post-username">${postUsername.textContent = posts[i].name}</h3>
-                        <p id="post-location">${postLocation.textContent = posts[i].location}</p>
-                    </div>
-                </div>
-                <img class="post-image" data-index="${i}" src="${postImage.src = posts[i].post}">
-                <div class="post-footer">
-                    <div class="post-icons">
-                        <img class="like-icon" data-index="${i}" src="images/icon-heart.png">
-                        <img id="comment-icon" src="images/icon-comment.png">
-                        <img id="dm-icon" src="images/icon-dm.png">
-                    </div>
-                    <p class="num-likes" id="likes-${i}">${posts[i].likes} likes</p>
-                    <!-- caption? -->
-                    <p><span id="user-id">${username.textContent = posts[i].username}</span> <span id="comment">${comment.textContent = posts[i].comment}</span></p>
-                </div>
-            </section>`
+document.addEventListener('dblclick', function(e){
+    if(e.target.dataset.imageLike){
+        handleDblClick(e.target.dataset.imageLike)
     }
+})
+
+function handleDblClick(imageId){
+    const imageObj = posts.filter(function(likes){
+        return likes.uuid === imageId
+    })[0]
+
+    if (imageObj.isLiked){
+        imageObj.likes--
+    } else {
+        imageObj.likes++
+    }
+    imageObj.isLiked = !imageObj.isLiked
+    render()
 }
 
-// Used classList.contains method to target intended image and .getAttribute method to target unique indexed icons within the loop
-feed.addEventListener("click", function(){
-    if(event.target.classList.contains("like-icon")) {
-        let index = event.target.getAttribute("data-index");
+function handleLikeClick(postsId){
+    const postsObj = posts.filter(function(likes){
+        return likes.uuid === postsId
+    })[0]
 
-        posts[index].likes++
-
-        document.getElementById(`likes-${index}`).textContent = `${posts[index].likes} likes`
+    if (postsObj.isLiked){
+        postsObj.likes--
+    } else {
+        postsObj.likes++
     }
-})
+    postsObj.isLiked = !postsObj.isLiked
+    render()
+}
 
-feed.addEventListener("dblclick", function(){
-    if(event.target.classList.contains("post-image")) {
-        let index = event.target.getAttribute("data-index");
+function getPostsHtml(){
+    let postHtml = ''
 
-        posts[index].likes++
+    posts.forEach(function(post){
+        let likeClass = ''
+        let likeType = 'fa-regular'
 
-        document.getElementById(`likes-${index}`).textContent = `${posts[index].likes} likes`
-    }
-})
+        if (post.isLiked){
+            likeClass = 'liked'
+            likeType = 'fa-solid'
+        }
+
+        postHtml += `
+        <section class="container">
+                <div class="post-header">
+                    <img id="post-avatar" src="${post.avatar}">
+                    <div class="block">
+                        <h3 id="post-username">${post.name}</h3>
+                        <p id="post-location">${post.location}</p>
+                    </div>
+                </div>
+                <img class="post-image" data-image-like="${post.uuid}" src="${post.post}">
+                <div class="post-footer">
+                    <div class="post-icons">
+                        <i class="${likeType} fa-heart ${likeClass}" data-like="${post.uuid}"></i>
+                        <i class="fa-regular fa-comment"></i>
+                        <i class="fa-regular fa-paper-plane"></i>
+                    </div>
+                    <p class="num-likes" id="likes-${post.uuid}">${post.likes} likes</p>
+                    <!-- caption? -->
+                    <p><span id="user-id">${post.username}</span> <span id="comment">${post.comment}</span></p>
+                </div>
+            </section>`
+    })
+    return postHtml
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getPostsHtml()
+}
+
+render()
 
