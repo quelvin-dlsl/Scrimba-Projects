38b6edd421743de2d4ea531483a0f9ee9594bae1
quelvin-dlsl <qnaqyeon@gmail.com>
2025-12-21 02:05:35 +0400

    Added scripts for search function to work

--- movie_watchlist/index.js
+++ movie_watchlist/index.js
@@ -1,14 +1,35 @@
 // My API key:  8fbb449d
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-let count = 0
-let aboutMovie = []
+document.getElementById('search-movie').addEventListener('submit', async(e) => {
+    e.preventDefault();
 
-fetch('http://www.omdbapi.com/?t=Movie&apikey=8fbb449d')
-    .then(res => res.json())
-    .then(data => {
+    const searchTerm = document.querySelector('#input-movie').value;
 
-        for(const [key, value] of Object.entries(data)){
-            console.log(`${key}: ${value}`)
+    try {
+        const searchRes = await fetch(`http://www.omdbapi.com/?s=${searchTerm}&apikey=8fbb449d`)
+        const searchData = await searchRes.json()
+
+        if(!searchData.Search){
+            document.getElementById('movie-list').innerHTML = '<p class="no-movie">No movies found</p>'
+            return
         }
-    })
+
+        const movieDetailsPromises = searchData.Search.map(movie => {
+            return fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=8fbb449d`)
+                .then(res => res.json())
+        })
+
+        const movies = await Promise.all(movieDetailsPromises)
+
+        const html = movies.map(movie => `
+            <div class="movie-card">
+                <h4>${movie.Title}</h4>
+                <h3>${movie.Year}</h3>
+            </div>`).join('')
+
+        document.getElementById('movie-list').innerHTML = html
+    } catch (error) {
+        console.log('Error fetching movies: ', error)
+    }
+})
